b4998996ce37f9874a124e9c06327ffe812a1285
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-11-24 21:22:44 +0000

    2015 day 14 p1

--- src/2015/14/index.ts
+++ src/2015/14/index.ts
@@ -1,5 +1,38 @@
 import { fromFileUrl, dirname } from 'https://deno.land/std@0.105.0/path/mod.ts';
 const input = Deno.readTextFileSync(`${dirname(fromFileUrl(import.meta.url))}\\input.txt`).split('\n') as string[];
 
-function p1() {}
+function p1() {
+	const reg: { [key: string]: { speed: number; length: number; cooldown: number } } = {};
+	for (let x of input) {
+		const i = x.split(' ');
+		reg[i[0]] = { speed: parseInt(i[3]), length: parseInt(i[6]), cooldown: parseInt(i.at(-2) as string) };
+	}
+
+	let highest = 0;
+
+	for (let x in reg) {
+		let seconds = 2503;
+		let distance = 0;
+		const i = reg[x];
+
+		while (seconds > 0) {
+			if (seconds < i.length) {
+				distance += seconds * i.speed;
+				break;
+			}
+
+			distance += i.length * i.speed;
+			seconds -= i.length;
+
+			if (seconds <= i.cooldown) break;
+			seconds -= i.cooldown;
+		}
+
+		if (distance > highest) highest = distance;
+	}
+
+	console.log(highest);
+}
 function p2() {}
+
+p1();
